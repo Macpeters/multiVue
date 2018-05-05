ffc596c694315783f67f356157cca6274564f101
Macpeters <peters.maryanne@gmail.com>
2018-05-05 14:15:41 -0400

    add generate rule

---
+++ config.js
@@ -0,0 +1,27 @@
+build: {
+  rules: {
+    images: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: 'url-loader',
+        exclude: /(assets\/svg)/,
+        query: {
+          limit: 1000, // 1KO
+          name: 'img/[name].[hash:7].[ext]',
+        },
+      },
+      {
+        test: /\.svg$/,
+        include: [
+          path.resolve(__dirname, 'assets/svg'),
+        ],
+        use: 'svg-sprite-loader',
+      }
+    ]
+  }
+}
+generate: {
+  minify: {
+    collapseWhitespace: false
+  }
+}
