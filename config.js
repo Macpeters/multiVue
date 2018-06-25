dcbb7d0b36f52664ed03ada57b2f60848982b2e4
Macpeters <peters.maryanne@gmail.com>
2018-06-25 17:40:00 -0400

    try a different google analytics thing

--- config.js
+++ config.js
@@ -2,4 +2,13 @@ generate: {
   minify: {
     collapseWhitespace: false
   }
+},
+{
+  modules: [
+    ['@nuxtjs/google-analytics']
+  ],
+
+    'google-analytics': {
+    id: 'ZxmnP8EXJRjiayUi1MzTxW0p9D'
+  }
 }
