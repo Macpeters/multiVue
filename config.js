db0a083a03411dc019096fd945a4f172deee9566
Macpeters <peters.maryanne@gmail.com>
2018-07-07 20:28:16 -0400

    try the js way again

--- config.js
+++ config.js
@@ -4,17 +4,22 @@ generate: {
   }
 }
 module.exports = {
-  head: {
-    script: [
-      { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' }
-    ],
-    link: [
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' }
-    ]
-  },
-  modules: [
-    ['@nuxtjs/google-analytics', { id: 'UA-113895926-1' }]
-  ],
+  plugins: [
+    { src: '~plugins/ga.js', ssr: false }
+  ]
+}
+// module.exports = {
+//   head: {
+//     script: [
+//       { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' }
+//     ],
+//     link: [
+//       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' }
+//     ]
+//   },
+//   modules: [
+//     ['@nuxtjs/google-analytics', { id: 'UA-113895926-1' }]
+//   ],
   // plugins: [
   //   { src: '~plugins/ga.js', ssr: false }
   // ]
--- pages/murals.vue
+++ pages/murals.vue
@@ -151,7 +151,7 @@ export default {
         { hid: `og:image`, property: 'og:image', content: octoBuddy_thumb }
       ],link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
       script: [
-        // these scripts cost load time
+        // these scripts cost time
         // { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' }
       ],
       link: [
