4d541e801eb9f245437848f0d81d75d78f0ee2cd
Macpeters <peters.maryanne@gmail.com>
2018-07-29 13:40:30 -0400

    ...

--- config.js
+++ config.js
@@ -8,7 +8,6 @@ generate: {
     // Simple usage
     ['@nuxtjs/google-analytics', {
       id: 'UA-113895926-1'
-      // id: 'ZxmnP8EXJRjiayUi1MzTxW0p9D'
     }]
   ]
 }
@@ -16,20 +15,4 @@ module.exports = {
   plugins: [
     { src: '~plugins/ga.js', ssr: false }
   ]
-}
-// module.exports = {
-//   head: {
-//     script: [
-//       { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' }
-//     ],
-//     link: [
-//       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' }
-//     ]
-//   },
-//   modules: [
-//     ['@nuxtjs/google-analytics', { id: 'UA-113895926-1' }]
-//   ],
-  // plugins: [
-  //   { src: '~plugins/ga.js', ssr: false }
-  // ]
 }
